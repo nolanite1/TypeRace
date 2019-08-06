29c51a710cda72517d145c0d20c2d57477bef9f5
Sailesh Sai Sirigineedi <sai2468@Saileshs-MacBook-Pro.local>
2019-08-06 13:34:51 -0500

    first commit

---
+++ venv/app.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://play.typeracer.com')
+time.sleep(3)
+linkElem = driver.find_element_by_link_text('Enter a typing race')
+linkElem.click()
+time.sleep(3)
+firstLetterElement = driver.find_element_by_xpath('//span[@unselectable="on"]')
+letter = firstLetterElement.text
+restOfWords = driver.find_elements_by_xpath('//span[@unselectable="on"]')
+print(len(restOfWords))
+stuffToType = ''
+firstWord = ''
+for x in range(0,len(restOfWords)-1):
+    if len(restOfWords[0].text) == 1:
+             firstWord+=restOfWords[x].text
+print(firstWord)
+for x in range(1,len(restOfWords)):
+    stuffToType = firstWord + " " + restOfWords[x].text
+print(stuffToType)
+time.sleep(11)
+
+inputBox = driver.find_element_by_class_name("txtInput")
+inputBox.send_keys(stuffToType)
